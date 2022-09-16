2c3b862f3c3a88012fb911c6b3a4114e661fb2d1
JoaoMarceloDMSilva <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2022-09-16 10:19:05 -0300

    Questão 4
    Quantidade de folhas de uma árvore

---
+++ questao_4.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct nodo {
+  int value;
+  struct nodo *left;
+  struct nodo *right;
+};
+
+typedef struct  nodo Nodo;
+
+Nodo* create(int num){
+  Nodo *no = malloc(sizeof(Nodo));
+  no -> value = num;
+  no -> left = NULL;
+  no -> right = NULL;
+  return no;
+}
+
+void to_print (Nodo *n) {
+  if (n == NULL) {
+    return;
+  }
+  printf("\n%d", n->value);
+  to_print(n->left);
+  to_print(n->right);
+}
+
+void add (Nodo *n, int num) {
+  if (num == n->value){
+    return;
+  }
+
+  if (num < n->value){
+
+    if (n->left == NULL){
+      n->left = create(num);
+    }
+    else {
+      add(n->left, num);
+    }
+
+  }else{
+
+    if (n -> right == NULL){
+      n->right = create(num);
+    }
+    else {
+      add(n->right, num);
+    }
+  }
+}
+
+int counting_leaf (Nodo *root){
+  if (root == NULL){
+    return 0;
+  } else if(root->right == NULL && root->left == NULL){
+    return 1;
+  }else{
+    return counting_leaf(root->right) + counting_leaf(root->left);
+  }
+}
+
+Nodo* free_Tree (Nodo *root){
+  if (root == NULL){
+    return 0;
+  }
+  free_Tree(root->left);
+  free_Tree(root->right);
+  free(root);
+  root = NULL;
+}
+
+
+int main () {
+  int op, start, info, quant_leaf;
+
+  printf("PASSO INICIAL:\nInsira um valor inteiro para a raíz: ");
+  scanf("%d", &start);
+  Nodo *root = create(start);
+
+  do{
+    printf("\n\n1 - Adicionar valor\n2 - Imprimir árvore\n3 - Quantidade de nós FOLHA\n0 - Sair\nOpção: ");
+    scanf("%d", &op);
+    switch(op){
+      case 1:
+        printf("\nInsira um valor inteiro para adicionar: ");
+        scanf("%d", &info);
+        add(root, info);
+      break;
+
+      case 2:
+        to_print(root);
+      break;
+
+      case 3:
+        quant_leaf = counting_leaf(root);
+        printf("\nA árvore possui %d nós FOLHA",quant_leaf);
+      break;
+
+      case 0:
+        free_Tree(root);
+        printf("\nSaindo ...\n");
+      break;
+
+      default:
+        printf("\nERRO: Opção inválida!\n");
+
+    }
+  }while(op != 0);
+
+  
+  return 0;
+}
